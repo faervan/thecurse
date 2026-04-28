aecb75f1bc0be3cdfff2a1cd0a27b5f4147db9ca
faervan <faervan@proton.me>
2026-04-29 00:55:46 +0200

    feat: add improve debug toggle system and add physics debug gizmo

--- core/src/debug.rs
+++ core/src/debug.rs
@@ -5,40 +5,120 @@ use bevy_inspector_egui::{bevy_egui::EguiPlugin, quick::WorldInspectorPlugin};
 use crate::prelude::*;
 
 pub fn plugin(app: &mut App) {
-    app.insert_state(DebugElementActive::<WorldInspectorPlugin>::default());
-    app.add_plugins((
-        EguiPlugin::default(),
-        WorldInspectorPlugin::default()
-            .run_if(in_state(DebugElementActive::<WorldInspectorPlugin>::ACTIVE)),
-    ));
-
-    app.add_systems(Update, toggle_debug_element::<3, WorldInspectorPlugin>);
+    WorldInspectorPlugin::debug_plugin(app);
+    PhysicsDebugPlugin::debug_plugin(app);
 }
 
-const fn key_map(id: usize) -> KeyCode {
-    match id {
-        3 => KeyCode::F3,
-        _ => panic!(),
+trait DebugElement: Sized + Send + Sync + 'static {
+    const KEY: KeyCode;
+
+    fn debug_plugin(app: &mut App) -> &mut App {
+        app.insert_state(DebugElementActive::<Self>::default());
+        app.add_systems(Update, Self::toggle_on_key_press);
+        Self::plugin(app);
+        if let Some(on_enable) = Self::on_enable() {
+            app.add_systems(OnEnter(DebugElementActive::<Self>::ACTIVE), on_enable);
+        }
+        if let Some(on_disable) = Self::on_disable() {
+            app.add_systems(OnExit(DebugElementActive::<Self>::ACTIVE), on_disable);
+        }
+        app
+    }
+
+    #[allow(unused_variables)]
+    fn plugin(app: &mut App) {}
+
+    fn on_enable() -> Option<impl System<In = (), Out = ()>> {
+        if false {
+            return Some(IntoSystem::into_system(|| {}));
+        }
+        None
+    }
+
+    fn on_disable() -> Option<impl System<In = (), Out = ()>> {
+        if false {
+            return Some(IntoSystem::into_system(|| {}));
+        }
+        None
+    }
+
+    fn toggle_on_key_press(
+        input: Res<ButtonInput<KeyCode>>,
+        state: Res<State<DebugElementActive<Self>>>,
+        mut next_state: ResMut<NextState<DebugElementActive<Self>>>,
+    ) {
+        if input.just_pressed(Self::KEY) {
+            next_state.set(DebugElementActive::new(!state.get().active));
+        }
     }
 }
 
-fn toggle_debug_element<const KEY: usize, T: Send + Sync + 'static>(
-    input: Res<ButtonInput<KeyCode>>,
-    state: Res<State<DebugElementActive<T>>>,
-    mut next_state: ResMut<NextState<DebugElementActive<T>>>,
-) {
-    if input.just_pressed(key_map(KEY)) {
-        next_state.set(DebugElementActive::new(!state.get().active));
+macro_rules! on_enable {
+    ($system:expr) => {
+        fn on_enable() -> Option<impl System<In = (), Out = ()>> {
+            Some(IntoSystem::into_system($system))
+        }
+    };
+}
+
+macro_rules! on_disable {
+    ($system:expr) => {
+        fn on_disable() -> Option<impl System<In = (), Out = ()>> {
+            Some(IntoSystem::into_system($system))
+        }
+    };
+}
+
+impl DebugElement for WorldInspectorPlugin {
+    const KEY: KeyCode = KeyCode::F1;
+
+    fn plugin(app: &mut App) {
+        app.add_plugins((
+            EguiPlugin::default(),
+            WorldInspectorPlugin::default()
+                .run_if(in_state(DebugElementActive::<WorldInspectorPlugin>::ACTIVE)),
+        ));
     }
 }
 
-#[derive(States, Default)]
-struct DebugElementActive<T: Send + Sync + 'static> {
+impl DebugElement for PhysicsDebugPlugin {
+    const KEY: KeyCode = KeyCode::F4;
+
+    fn plugin(app: &mut App) {
+        app.add_plugins(Self);
+        app.insert_gizmo_config(
+            PhysicsGizmos::default(),
+            GizmoConfig {
+                enabled: false,
+                ..Default::default()
+            },
+        );
+    }
+
+    on_enable!(|mut configs: ResMut<GizmoConfigStore>| {
+        if let Some((gizmo_config, _physics_gizmo)) =
+            configs.get_config_mut_dyn(&std::any::TypeId::of::<PhysicsGizmos>())
+        {
+            gizmo_config.enabled = true;
+        }
+    });
+
+    on_disable!(|mut configs: ResMut<GizmoConfigStore>| {
+        if let Some((gizmo_config, _physics_gizmo)) =
+            configs.get_config_mut_dyn(&std::any::TypeId::of::<PhysicsGizmos>())
+        {
+            gizmo_config.enabled = false;
+        }
+    });
+}
+
+#[derive(States)]
+struct DebugElementActive<T: DebugElement> {
     active: bool,
     _phantom: PhantomData<T>,
 }
 
-impl<T: Send + Sync + 'static> DebugElementActive<T> {
+impl<T: DebugElement> DebugElementActive<T> {
     const ACTIVE: Self = Self {
         active: true,
         _phantom: PhantomData,
@@ -52,7 +132,16 @@ impl<T: Send + Sync + 'static> DebugElementActive<T> {
     }
 }
 
-impl<T: Send + Sync + 'static> Clone for DebugElementActive<T> {
+impl<T: DebugElement> Default for DebugElementActive<T> {
+    fn default() -> Self {
+        Self {
+            active: false,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<T: DebugElement> Clone for DebugElementActive<T> {
     fn clone(&self) -> Self {
         Self {
             active: self.active,
@@ -61,21 +150,21 @@ impl<T: Send + Sync + 'static> Clone for DebugElementActive<T> {
     }
 }
 
-impl<T: Send + Sync + 'static> Debug for DebugElementActive<T> {
+impl<T: DebugElement> Debug for DebugElementActive<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "DebugElementActive {{ active: {} }}", self.active)
     }
 }
 
-impl<T: Send + Sync + 'static> PartialEq for DebugElementActive<T> {
+impl<T: DebugElement> PartialEq for DebugElementActive<T> {
     fn eq(&self, other: &Self) -> bool {
         self.active == other.active
     }
 }
 
-impl<T: Send + Sync + 'static> Eq for DebugElementActive<T> {}
+impl<T: DebugElement> Eq for DebugElementActive<T> {}
 
-impl<T: Send + Sync + 'static> Hash for DebugElementActive<T> {
+impl<T: DebugElement> Hash for DebugElementActive<T> {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
         self.active.hash(state);
     }
