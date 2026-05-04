3a126da1445e6904fabf01e4a3fd4acc7a369b12
faervan <faervan@proton.me>
2026-05-04 02:45:02 +0200

    fix: follow warning when player despawns

--- core/src/utils/follow.rs
+++ core/src/utils/follow.rs
@@ -21,15 +21,22 @@ where
 
 #[derive(Component, Reflect)]
 #[reflect(Component)]
+#[relationship(relationship_target = FollowedBy)]
 /// This entity will "follow" the [`Follow.target`] entity.
 /// Following means its [`Transform.translation`] will by synced with the [`target`] entity.
-/// Entities that have this component cannot by a followed target themselves.
+/// Entities that have this component cannot be a followed target themselves.
 pub struct Follow {
+    #[relationship]
     /// The id of the [`Entity`] that is being followed
     pub target: Entity,
     /// The offset from the target entity. Any rotation will be disregarded.
     pub offset: Vec3,
 }
+
+#[derive(Component, Reflect)]
+#[reflect(Component)]
+#[relationship_target(relationship = Follow)]
+pub struct FollowedBy(Vec<Entity>);
 
 fn follow(
     followers: Query<(&mut Transform, &Follow)>,
@@ -38,9 +45,6 @@ fn follow(
     for (mut transform, follow) in followers {
         if let Ok(target_transform) = followed.get(follow.target) {
             transform.translation = target_transform.translation + follow.offset;
-        } else {
-            #[cfg(feature = "dev")]
-            warn!("Cannot retrieve target of the FollowC component!");
         }
     }
 }
